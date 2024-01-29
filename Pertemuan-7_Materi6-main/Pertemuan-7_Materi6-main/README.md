098d40305399c4e4eb86eb47313bf947c4f63036
icestrawberrysquash <158044946+icestrawberrysquash@users.noreply.github.com>
2024-01-29 18:43:36 +0700

    Add files via upload

---
+++ Pertemuan-7_Materi6-main/Pertemuan-7_Materi6-main/README.md
@@ -0,0 +1,2 @@
+source code dan output ![Screenshot (12)](https://github.com/Fandiy306/Petemuan-7_Materi6/assets/132991574/0055a447-b414-4ea6-92b2-3befb0850039)
+![Screenshot (11)](https://github.com/Fandiy306/Petemuan-7_Materi6/assets/132991574/e2abe5ea-570d-43c4-a062-612789a41097)
---
+++ Pertemuan-7_Materi6-main/Pertemuan-7_Materi6-main/tugas/implementasi.php
@@ -0,0 +1,13 @@
+<?php
+$nilai = 75;
+
+if ($nilai >= 80) {
+    echo "Selamat! Anda mendapatkan nilai A.";
+} elseif ($nilai >= 70) {
+    echo "Anda mendapatkan nilai B.";
+} elseif ($nilai >= 60) {
+    echo "Anda mendapatkan nilai C.";
+} else {
+    echo "Maaf, Anda tidak lulus.";
+}
+?>
